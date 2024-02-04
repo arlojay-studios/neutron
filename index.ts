34bf2de555845dd44ce60d427726c3dd82f535be
Source <65256422+RealSourceOfficial@users.noreply.github.com>
2024-02-04 12:28:39 -0800

    protonServer constructor

--- index.ts
+++ index.ts
@@ -15,16 +15,23 @@ import { electron } from '@arlojay-studios/electron-atomic/web';
 
 export class protonServer {
     private server: Application = require('express')()
+    private db: protonDB;
+    private validator: protonUUID;
+    private web: electron;
 
-    public async init(port: number, dbPath: string): Promise<typeof db> {
+    constructor(dbPath: string) {
+        this.db = new protonDB(dbPath);
+        this.validator = new protonUUID();
+        this.web = new electron();
+    }
 
-        const db = new protonDB(dbPath);
-        const validatior = new protonUUID();
-        const web = new electron();
+    public async init(port: number): Promise<typeof this.db> {
+        
+        
 
         try {
-            await db.open();
-            await db.run(`
+            await this.db.open();
+            await this.db.run(`
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY
                 uuid TEXT
@@ -33,18 +40,18 @@ export class protonServer {
             console.error();
             return error;
         } finally {
-            await db.close();
+            await this.db.close();
         }
 
         return new Promise((resolve, reject) => {
             this.server.use(async (req: Request, res: Response, next) => {
                 const clientUUID = req.get('client-uuid');
                 try {
-                    await db.open();
+                    await this.db.open();
                     if (clientUUID) {
                         if (/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(clientUUID)) {
-                            if (!await validatior.isClientInProtonDB(db, clientUUID)) {
-                                validatior.storeClientIdInProtonDB(db, clientUUID);
+                            if (!await this.validator.isClientInProtonDB(this.db, clientUUID)) {
+                                this.validator.storeClientIdInProtonDB(this.db, clientUUID);
                                 res.send(/* Notify user data needs to be fetched from upstream, as
                                 if client id valid
                                 but not in server
@@ -54,31 +61,31 @@ export class protonServer {
                             res.status(400).json({ error: ">:l don't spoof ClientIDs." })
                         }
                     } else {
-                        const clientUUID = validatior.generateClientId()
-                        validatior.storeClientIdInProtonDB(db, clientUUID);
+                        const clientUUID = this.validator.generateClientId()
+                        this.validator.storeClientIdInProtonDB(this.db, clientUUID);
                         res.send(/* Send user their new uuid */)
                     }
                 } catch (err) {
                     res.status(500).json({ error: 'Internal Server Error' });
                     reject(err);
                 } finally {
-                    await db.close();
+                    await this.db.close();
                 }
                 next();
             });
 
             this.server.get('/', (req: Request, res: Response) => {
                 const clientUUID = req.get('client-uuid')
-                web.mainPage(res);
+                this.web.mainPage(res);
             });
 
             this.server.get('/web.ts', (req: Request, res: Response) => {
-                web.script(res);
+                this.web.script(res);
             });
 
             this.server.listen(port, () => {
                 console.log(`Neutron is running on ${port}`);
-                resolve(db);
+                resolve(this.db);
             });
         }
         )
