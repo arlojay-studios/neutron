94878b051ba5be20de5533d4a72d9a1fb2fe294e
Source <65256422+RealSourceOfficial@users.noreply.github.com>
2024-02-10 14:26:46 -0800

    some casual dev work

--- index.ts
+++ index.ts
@@ -4,20 +4,21 @@
  * Module Imports
  */
 
-import type { Response, Request, Application } from 'express';
-import { protonDB, protonUUID } from '@arlojay-studios/proton-atomic/core'
-import { electron } from '@arlojay-studios/electron-atomic/web';
+import type { Application } from 'express';
+import { cookieParser } from 'cookieParser'
+import { protonDB, protonUUID } from '@arlojay-studios/proton-atomic/'
+import { electron } from '@arlojay-studios/electron-atomic/';
 
 /**
  * Request Handler
  * @public
  */
 
-export class neutronServer {
-    private server: Application = require('express')()
-    private db: protonDB;
-    private validator: protonUUID;
-    private web: electron;
+export class neutron {
+    private neutron: Application = require('express')()
+    private protonDB: protonDB;
+    private uuid: protonUUID;
+    private electron: electron;
 
     /**
      * Create a new server instance
@@ -25,81 +26,61 @@ export class neutronServer {
      */
 
     constructor(dbPath: string) {
-        this.db = new protonDB(dbPath);
-        this.validator = new protonUUID();
-        this.web = new electron();
+        this.protonDB = new protonDB(dbPath);
+        this.uuid = new protonUUID();
+        this.electron = new electron();
     }
 
     /**
      * Start the server and database
-     * @param port - Port the server should run on
+     * @param { number } port - Port the server should run on
      * @returns Server + Database handle
      */
 
-    public async init(port: number): Promise<[typeof this.server, typeof this.db]> {
+    public async init(port: number): Promise<[Application, protonDB]> {
 
         try {
-            await this.db.open();
-            await this.db.run(`
+            await this.protonDB.open();
+            await this.protonDB.run(`
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY,
                 uuid TEXT
             )`)
         } catch (error) {
-            console.error();
             Promise.reject(error);
         } finally {
-            await this.db.close();
+            await this.protonDB.close();
         }
 
         return new Promise((resolve, reject) => {
-            this.server.use(async (req: Request, res: Response, next) => {
-                const clientUUID = req.get('client-uuid');
-                try {
-                    await this.db.open();
-                    if (clientUUID) {
-                        if (/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(clientUUID)) {
-                            if (!await this.validator.isClientInProtonDB(this.db, clientUUID)) {
-                                this.validator.storeClientIdInProtonDB(this.db, clientUUID);
-                                res.send(/* Notify user data needs to be fetched from upstream, as
-                                if client id valid
-                                but not in server
-                                fetch from master */)
-                            }
-                        } else {
-                            res.status(400).json({ error: ">:l don't spoof ClientIDs." })
-                        }
-                    } else {
-                        const clientUUID = this.validator.generateClientId()
-                        this.validator.storeClientIdInProtonDB(this.db, clientUUID);
-                        res.send(/* Send user their new uuid */)
-                    }
-                } catch (err) {
-                    res.status(500).json({ error: 'Internal Server Error' });
-                } finally {
-                    await this.db.close();
-                    next();
-                }
-            });
 
-            this.server.get('/', (res: Response) => {
-                /* const clientUUID = req.get('client-uuid') */
-                this.web.mainPage(res);
-            });
+            // wip 
+            this.neutron.use(cookieParser())
 
-            this.server.get('/web.ts', (res: Response) => {
-                this.web.script(res);
+            this.electron.page(this.neutron)
+
+            //wip
+            this.neutron.get('/userlog', (req, res) => {
+                console.log(req.cookies)
             });
 
             try {
-                this.server.listen(port, () => {
+                this.neutron.listen(port, () => {
                     console.log(`Neutron is running on ${port}`);
-                    return resolve([this.server, this.db]);
+                    return resolve([this.neutron, this.protonDB]);
                 });
             } catch (err) {
                 reject(err);
             }
         }
         )
+    }
+
+    public async validateUUID(clientUUID: string) {
+        
+    }
+
+    public async newUUID() {
+
     }
 }
